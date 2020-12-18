c240f9864638a258e9f4b44e38f5b0248bc4a9ed
unknown <jeffputnam800@gmail.com>
2020-12-18 14:43:21 -0500

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blue[900],
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/grinch.png'),
+              ),
+              Text(
+                'Jeff Putnam',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.grey[200],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'SYSTEM TECHNICIAN',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro-Black.ttf',
+                  fontSize: 20.0,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
